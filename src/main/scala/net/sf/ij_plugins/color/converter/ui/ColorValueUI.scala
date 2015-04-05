dbee1499caa4033b9998447ed04dee5b9998e000
Jarek Sacha <jpsacha@gmail.com>
2015-04-04 22:57:01 -0400

    Simplify implementation using new TextFormatter.

--- src/main/scala/net/sf/ij_plugins/color/converter/ui/ColorValueUI.scala
+++ src/main/scala/net/sf/ij_plugins/color/converter/ui/ColorValueUI.scala
@@ -26,34 +26,44 @@ import java.text.DecimalFormat
 
 import net.sf.ij_plugins.color.converter.ColorTriple
 import net.sf.ij_plugins.color.converter.ColorTriple.Color123
+import net.sf.ij_plugins.color.converter.ui.ColorValueUI.NumberTextField
 
 import scalafx.Includes._
 import scalafx.beans.property.ObjectProperty
 import scalafx.geometry.Pos
+import scalafx.scene.control.{TextField, TextFormatter}
 import scalafx.scene.layout.HBox
+import scalafx.util.converter.FormatStringConverter
 
+object ColorValueUI {
+  private class NumberTextField extends TextField {
+    private val format = new DecimalFormat("0.000000")
+    private val converter = new FormatStringConverter[Number](format)
+    val model = new TextFormatter(converter)
+    textFormatter = model
+  }
+}
 
 /** Row of number text fields holding color band values. */
 class ColorValueUI {
 
-  private val format = new DecimalFormat("0.000000")
-  private val band1NTF = new DoubleNumberTextField(0, format)
-  private val band2NTF = new DoubleNumberTextField(0, format)
-  private val band3NTF = new DoubleNumberTextField(0, format)
+  private val band1NTF = new NumberTextField()
+  private val band2NTF = new NumberTextField()
+  private val band3NTF = new NumberTextField()
   private val allBands = List(band1NTF, band2NTF, band3NTF)
   private var updating = false
 
   allBands.foreach(_.prefColumnCount = 6)
   allBands.foreach(_.setAlignment(Pos.CenterRight))
-  allBands.foreach(_.numberProperty().onChange(updateColor()))
+  allBands.foreach(_.textFormatter().value.onChange(updateColor()))
 
   val color = new ObjectProperty[ColorTriple](this, "color", new Color123())
   color.onChange {
     updating = true
     try {
-      band1NTF.setNumber(color()(0))
-      band2NTF.setNumber(color()(1))
-      band3NTF.setNumber(color()(2))
+      band1NTF.model.value() = color()(0)
+      band2NTF.model.value() = color()(1)
+      band3NTF.model.value() = color()(2)
     } finally {
       updating = false
     }
@@ -66,7 +76,12 @@ class ColorValueUI {
 
   private def updateColor() {
     if (!updating) {
-      color() = new Color123(band1NTF.getNumber, band2NTF.getNumber, band3NTF.getNumber)
+      val numbers = allBands.map(_.model.value())
+      // Check if ready
+      if (numbers.contains(null)) return
+
+      val doubles = numbers.map(_.doubleValue())
+      color() = new Color123(doubles(0), doubles(1), doubles(2))
     }
   }
 }
