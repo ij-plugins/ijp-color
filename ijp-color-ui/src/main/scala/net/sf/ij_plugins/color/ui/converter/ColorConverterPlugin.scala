29b6e8d10e54063e854b776e67c9da71200ca557
Jarek Sacha <jpsacha@gmail.com>
2021-01-23 15:51:29 -0500

    Move all in ijp-color-ui to its own package `net.sf.ij_plugins.color.ui` to avoid conflicts

---
+++ ijp-color-ui/src/main/scala/net/sf/ij_plugins/color/ui/converter/ColorConverterPlugin.scala
@@ -0,0 +1,85 @@
+/*
+ * Image/J Plugins
+ * Copyright (C) 2002-2021 Jarek Sacha
+ * Author's email: jpsacha at gmail dot com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Latest release available at https://github.com/ij-plugins/ijp-color/
+ */
+
+package net.sf.ij_plugins.color.ui.converter
+
+import ij.IJ
+import ij.plugin.PlugIn
+import net.sf.ij_plugins.color.ui.fx.{ColorFXUI, imageJIconAsFXImage, initializeFX}
+import org.scalafx.extras.onFX
+import scalafx.Includes._
+import scalafx.scene.Scene
+import scalafx.stage.Stage
+
+import scala.util.control.NonFatal
+
+object ColorConverterPlugin {
+  private var dialogStage: Option[Stage] = None
+}
+
+/** Simple color conversion calculator. */
+class ColorConverterPlugin extends PlugIn {
+
+  private final val Title = "IJP Color Calculator"
+
+
+  def run(arg: String): Unit = {
+
+    try {
+      initializeFX()
+    } catch {
+      // ImageJ has a bug where is may show incorrect error message about missing plugin,
+      // se wee need to handle this here
+      case NonFatal(ex) =>
+        IJ.error(Title, "Cannot initialize JavaFX: " + ex.getMessage)
+        return
+      case ex: ClassNotFoundException =>
+        IJ.error(Title, "Cannot initialize JavaFX. ClassNotFoundException: " + ex.getMessage)
+        return
+      case ex: NoClassDefFoundError =>
+        IJ.error(Title, "Cannot initialize JavaFX. NoClassDefFoundError: " + ex.getMessage)
+        return
+      case ex: Throwable =>
+        IJ.error(Title, "Cannot initialize JavaFX: " + ex.getClass.getName + ": " + ex.getMessage)
+        return
+    }
+
+    onFX {
+      if (ColorConverterPlugin.dialogStage.isDefined) {
+        ColorConverterPlugin.dialogStage.foreach(_.show())
+      } else {
+        ColorConverterPlugin.dialogStage = Some(
+          new Stage {
+            title = Title
+            scene = new Scene {
+              title = Title
+              imageJIconAsFXImage.foreach(icons += _)
+              root = new ColorConverterView(new ColorConverterModel()).pane
+              stylesheets ++= ColorFXUI.stylesheets
+            }
+          }
+        )
+        ColorConverterPlugin.dialogStage.foreach(_.show())
+      }
+    }
+  }
+}
---
+++ ijp-color-ui/src/main/scala/net/sf/ij_plugins/color/ui/fx/ColorFXUI.scala
@@ -0,0 +1,70 @@
+/*
+ * Image/J Plugins
+ * Copyright (C) 2002-2021 Jarek Sacha
+ * Author's email: jpsacha at gmail dot com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Latest release available at https://github.com/ij-plugins/ijp-color/
+ */
+
+package net.sf.ij_plugins.color.ui.fx
+
+import scalafx.application.Platform
+import scalafx.scene.layout.StackPane
+import scalafx.scene.{Node, Scene}
+import scalafx.stage.Stage
+
+import java.net.URL
+
+/** Helper methods for creation of UI in `net.sf.ij_plugins.color` module. */
+object ColorFXUI {
+
+  /**
+    * Return string representing URL to stylesheet used by `ColorFXUI` user interface.
+    *
+    * Returns empty string if stylesheet is not present.
+    */
+  def stylesheets: Seq[String] = List(
+    "ijp-color.css"
+  ).flatMap(check(_).map(_.toExternalForm))
+
+  /**
+    * Show `node` in a new window.
+    *
+    * @param node        node to show.
+    * @param windowTitle window title.
+    */
+  def showInNewWindow(node: Node, windowTitle: String): Unit = {
+    Platform.runLater {
+      val dialogStage = new Stage() {
+        title = windowTitle
+        imageJIconAsFXImage.foreach(icons += _)
+        scene = new Scene {
+          root = new StackPane {
+            children = node
+            stylesheets ++= ColorFXUI.stylesheets
+          }
+        }
+      }
+      dialogStage.show()
+    }
+  }
+
+  private def check(name: String): Option[URL] = {
+    val stylesheetURL = getClass.getResource(name)
+    Option(stylesheetURL)
+  }
+}
