9d014cd578deb0a4695e9d80174e85c557cb9255
Jarek Sacha <jpsacha@gmail.com>
2022-07-12 23:05:41 -0400

    Correct for change in scalafx-extras API

--- ijp-color-ui/src/main/scala/ij_plugins/color/ui/fx/package.scala
+++ ijp-color-ui/src/main/scala/ij_plugins/color/ui/fx/package.scala
@@ -1,6 +1,6 @@
 /*
  * Image/J Plugins
- * Copyright (C) 2002-2021 Jarek Sacha
+ * Copyright (C) 2002-2022 Jarek Sacha
  * Author's email: jpsacha at gmail dot com
  *
  * This library is free software; you can redistribute it and/or
@@ -83,7 +83,7 @@ package object fx {
             println("FX handler caught exception: " + e.getMessage)
             e.printStackTrace()
             onFX {
-              showException("ImageJ FX Exception Handler", "Unexpected error.", e)
+              ShowMessage.exception("ImageJ FX Exception Handler", "Unexpected error.", e)
             }
           }
         }
